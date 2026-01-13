0a78e2fb4fcc0129850da431c2e518765e92e4f3
chanddwi780-cpu <chanddwi780@gmail.com>
2026-01-13 19:01:47 +0700

    fix: move fakerphp/faker to require and use \->faker in BookFactory to avoid undefined function error

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -18,17 +18,17 @@ class BookFactory extends Factory
     {
         // Generate a random category to make the image slightly more relevant if possible, 
         // though loremflickr /book is generic.
-        $category = fake()->randomElement(['Teknologi', 'Novel', 'Sains', 'Sejarah', 'Bisnis', 'Psikologi']);
+        $category = $this->faker->randomElement(['Teknologi', 'Novel', 'Sains', 'Sejarah', 'Bisnis', 'Psikologi']);
         
         return [
-            'title' => ucwords(fake()->words(3, true)), // "The Great Gatsby" style
-            'author' => fake()->name(),
-            'description' => fake()->paragraph(3),
+            'title' => ucwords($this->faker->words(3, true)), // "The Great Gatsby" style
+            'author' => $this->faker->name(),
+            'description' => $this->faker->paragraph(3),
             'category' => $category,
-            'stock' => fake()->numberBetween(3, 20),
+            'stock' => $this->faker->numberBetween(3, 20),
             // We use 'lock' param to ensure the image stays the same for this specific book entry
             // otherwise scrolling in Flutter might cause the image to refresh/change.
-            'cover_image' => 'https://loremflickr.com/300/450/book?lock=' . fake()->unique()->numberBetween(1, 10000),
+            'cover_image' => 'https://loremflickr.com/300/450/book?lock=' . $this->faker->unique()->numberBetween(1, 10000),
             'book_file' => null, // We don't have real PDFs for dummies
             'status' => 'Available',
         ];
