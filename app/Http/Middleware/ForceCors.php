11603d194d46a0bb84c97b631757f679ab6139c5
chanddwi780-cpu <chanddwi780@gmail.com>
2026-01-19 16:51:42 +0700

    fix: force global CORS middleware

---
+++ app/Http/Middleware/ForceCors.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceCors
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->isMethod('OPTIONS')) {
+            return response('', 200)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', '*');
+        }
+
+        $response = $next($request);
+
+        if (method_exists($response, 'header')) {
+            $response->header('Access-Control-Allow-Origin', '*')
+                     ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                     ->header('Access-Control-Allow-Headers', '*');
+        }
+
+        return $response;
+    }
+}
