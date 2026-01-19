135cd4c5b2046428f7d740d80f40947473f61371
chanddwi780-cpu <chanddwi780@gmail.com>
2026-01-19 16:37:46 +0700

    debug: add X-Backend-Version header

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: *");
+header("X-Backend-Version: 3.0"); // Version tag to verify update
 
 if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
     http_response_code(200);
